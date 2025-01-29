387439783cf6025c74a4118c755f0a60a69115ea
Alexander Komla <kunalexander360@gmail.com>
2025-01-29 08:18:16 +0000

    Random tensors

--- 01_pytorch_fundamentals.py
+++ 01_pytorch_fundamentals.py
@@ -21,7 +21,7 @@ scalar = torch.tensor(7)
 # print(scalar.item()) # Get tensor back as int  
 
 # VECTORS 
-#vector = torch.tensor([7, 7])  
+vector = torch.tensor([7, 7])  
 #print(vector.ndim) # 
 #print(vector.shape) # 2  
 
@@ -41,4 +41,31 @@ TENSOR = torch.tensor([
                        ]) 
 #print(TENSOR.ndim) #3 
 #print(TENSOR.shape) #[2, 2, 3]
-print(TENSOR[0][1][2])
+#print(TENSOR[0][1][2]) # 6   
+
+# RANDOM TENSORS 
+"""Random tensors are important because the way neural networks learn is that they start 
+tensors full of random numbers and adjust those random numbers to better represent the data
+
+Start with random numbers --> look at data --> update random numbers --> look at data --> update random numbers
+"""
+
+# Create random tensor of size (3, 4) 
+
+random_tensor = torch.rand(3, 4) 
+#print(random_tensor) 
+
+
+# Create a random tensor with similar shape to an image 
+random_image_size_tensor = torch.rand(size=(224, 224, 3)) # Height, width, color channels (R, G, B) 
+#print(random_image_size_tensor.ndim) 
+#print(random_image_size_tensor.size())
+
+# Random tensor of all zeros 
+zeros = torch.zeros(size=(3, 4))
+print(zeros)
+
+# Random tensor of one 
+ones = torch.ones(size=(2, 4), dtype=torch.int32)
+print(ones)
+print(ones.dtype)
