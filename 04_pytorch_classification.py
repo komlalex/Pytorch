7a77262debf1b308c66edbb233122c547145a435
Alexander Komla <kunalexander360@gmail.com>
2025-02-01 07:14:33 +0000

    converting our data to from numpy arrays to tensors and splitting it into training and testing sets

--- 04_pytorch_classification.py
+++ 04_pytorch_classification.py
@@ -8,8 +8,10 @@ can multiple things as options)
 
 # Make classification data and get it ready 
 
+import torch
 import sklearn
 from sklearn.datasets import make_circles 
+from sklearn.model_selection import train_test_split
 import pandas as pd 
 import matplotlib.pyplot as plt
 
@@ -22,8 +24,8 @@ x, y = make_circles(n_samples,
                     random_state=42) 
 
 
-print(f"First 5 samples of x: \n {x[0:5]}")
-print(f"First 5 samples of y: \n {y[0:5]}")
+#print(f"First 5 samples of x: \n {x[0:5]}")
+#print(f"First 5 samples of y: \n {y[0:5]}")
 
 # Make a DataFrame of circles data 
 
@@ -37,10 +39,36 @@ circles = pd.DataFrame({"x1": x[:, 0],
 plt.scatter(x= x[:, 0],
             y=x[:, 1], 
             c=y,
-            cmap=plt.cm.RdBu)
-plt.show()
+            cmap=plt.cm.RdYlBu)
+#plt.show()
 
 """
 The dataset we're working with is often referred to as a toy dataset.
 That's it is small enough to experiment but still sizeable enough to practice the fundamentals
 """
+
+# Check input and output shapes 
+
+#print(x.shape, y.shape)
+#print(x.ndim, y.ndim) 
+
+# View the first example of features and labels 
+x_sample = x[0]
+y_sample = y[0] 
+
+#print(f"Values for one sample of x: {x_sample} and the same for y: {y_sample}") 
+#print(f"Shapes for one sameple of x: {x_sample.shape} and the same for y: {y_sample.shape}")
+
+# Convert our data into tensors and create train and test splits 
+x = torch.from_numpy(x).type(torch.float32) 
+y = torch.from_numpy(y).type(torch.float32)
+
+torch.manual_seed(42)
+# Split data into training and test sets
+x_train, x_test, y_train, y_test = train_test_split(x,
+                                                     y, 
+                                                     test_size=0.2,
+                                                    random_state=42) # 20% for test, 80% for train
+
+
+print(len(y_train), len(y_test))
