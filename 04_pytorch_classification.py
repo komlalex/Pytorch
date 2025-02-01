40f6eb615fae04da240c89ad89efe105808370ac
Alexander Komla <kunalexander360@gmail.com>
2025-02-01 06:51:52 +0000

    Visualizing our data with matplotlib

--- 04_pytorch_classification.py
+++ 04_pytorch_classification.py
@@ -9,8 +9,9 @@ can multiple things as options)
 # Make classification data and get it ready 
 
 import sklearn
-
-from sklearn.datasets import make_circles
+from sklearn.datasets import make_circles 
+import pandas as pd 
+import matplotlib.pyplot as plt
 
 # Make 1000 samples 
 n_samples = 1000
@@ -25,9 +26,21 @@ print(f"First 5 samples of x: \n {x[0:5]}")
 print(f"First 5 samples of y: \n {y[0:5]}")
 
 # Make a DataFrame of circles data 
-import pandas as pd
+
 circles = pd.DataFrame({"x1": x[:, 0], 
                         "x2": x[:, 1],
                         "label": y}) 
 
-print(circles.head(10))
+#print(circles.head(10))
+
+# VISUALIZE 
+plt.scatter(x= x[:, 0],
+            y=x[:, 1], 
+            c=y,
+            cmap=plt.cm.RdBu)
+plt.show()
+
+"""
+The dataset we're working with is often referred to as a toy dataset.
+That's it is small enough to experiment but still sizeable enough to practice the fundamentals
+"""
