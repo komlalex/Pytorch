de1504dfe38c5aa4a1f537d96c842e04521d7c47
Alexander Komla <kunalexander360@gmail.com>
2025-02-03 09:29:55 +0000

    Improving our model's hyperparameters

--- 04_pytorch_classification.py
+++ 04_pytorch_classification.py
@@ -268,4 +268,101 @@ plt.subplot(1, 2, 2)
 plt.title("Test") 
 plot_decision_boundary(model_0, x_test, y_test) 
 
+#plt.show() 
+
+"""
+IMPROVING A MODEL (FROM A MODEL'S PERSPECTIVE)
+* Add more layers
+* Add more hidden units - g0 from 5 hidden units to 10 hidden units
+* Fit for longer 
+* Chaning the activation function
+* Change the learning rate
+* Change the loss function 
+These options are all from our model perspective because they deal diectly with the model, rather than the data.
+And because these options are all values we (as machine learning engineers and data scientists) change, they are referred to as hyperparameters.  
+
+Let's try and improve our model by: 
+1. Adding more hidden units: 5 -> 10
+2. Increasing the number of layers: 2 -> 3
+3. Increase the number of epochs: 100 -> 1000 
+
+"""
+
+class CircleModelV1(nn.Module): 
+    def __init__(self):
+        super().__init__() 
+        self.layer_1 = nn.Linear(in_features=2, out_features=10) 
+        self.layer_2 = nn.Linear(in_features=10, out_features=10) 
+        self.layer_3 = nn.Linear(in_features=10, out_features=1) 
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor: 
+        #z = self.layer_1(x) 
+        #z = self.layer_2(z) 
+        #z = self.layer_3(z) 
+        return self.layer_3(self.layer_2(self.layer_1(x))) # This way of writing operations leverages speed-ups where posible
+model_1 = CircleModelV1().to(device) 
+
+# Create a loss function 
+loss_fn = nn.BCEWithLogitsLoss()
+# Create an optimizer 
+optimizer = torch.optim.SGD(params=model_1.parameters(), lr=0.1)
+# Write a training and evaluation loop for model_1  
+
+torch.manual_seed(42) 
+torch.cuda.manual_seed(42)
+epochs = 1000 
+
+# Put data on device 
+x_train, y_train = x_train.to(device), y_train.to(device) 
+x_test, y_test = x_test.to(device), y_test.to(device)
+
+
+for epoch in range(epochs): 
+    model_1.train() 
+
+    # Forward pass
+    y_logits = model_1(x_train).squeeze()
+    y_pred = torch.round(torch.sigmoid(y_logits)) 
+
+    # Calcuate accuracy/loss 
+    loss = loss_fn(y_logits, y_train) 
+    acc = accuracy_fn(y_true=y_train, y_pred=y_pred)  
+
+    # Optimizer zero grad 
+    optimizer.zero_grad() 
+
+    # Loss backward 
+    loss.backward() 
+
+    #Optimizer step 
+    optimizer.step()
+
+    # TESTING
+    model_1.eval() 
+
+    with torch.inference_mode(): 
+
+        #Forward pass
+        test_logits = model_1(x_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))  
+
+        # Calculate accuracy/loss 
+        test_loss = loss_fn(test_logits, y_test)  
+        test_acc = accuracy_fn(y_true=y_test, y_pred=test_pred) 
+
+    # Print out what's happening
+    if epoch % 100 == 0: 
+        print(f"Epoch: {epoch} | Loss: {loss: .5f} | Acc: {acc:.2f}% | Test loss: {test_loss: .5f} | Test acc: {test_acc:.2f}%")
+
+"""
+Plot decision boundary
+"""
+plt.figure(figsize=(12, 6)) 
+plt.subplot(1, 2, 1)
+plt.title("Train") 
+plot_decision_boundary(model_1, x_train, y_train) 
+
+plt.subplot(1, 2, 2) 
+plt.title("Test") 
+plot_decision_boundary(model_1, x_test, y_test) 
 plt.show()
