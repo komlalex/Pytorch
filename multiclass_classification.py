6e2f76d66c8801c690ccd0713912e5669796ff38
Alexander Komla <kunalexander360@gmail.com>
2025-02-04 09:15:04 +0000

    Multi-class model created

--- multiclass_classification.py
+++ multiclass_classification.py
@@ -25,16 +25,50 @@ x_blob, y_blob = make_blobs(n_samples=1000,
 x_blob = torch.from_numpy(x_blob).type(torch.float32) 
 y_blob = torch.from_numpy(y_blob).type(torch.float32) 
 
+#print(torch.unique(y_train))
+
 # Create train and test split 
 x_train, x_test, y_train, y_test = train_test_split(x_blob,
                                                     y_blob,
                                                 test_size=0.2)  
 
 
-print(x_blob[:5])
-print(y_blob[:5])
 
 # Plot data 
 plt.figure(figsize=(10, 7)) 
-plt.scatter(x_blob[:, 0], x_blob[:,  1], c=y_blob, cmap =plt.cm.RdYlBu) 
-plt.show()
+#plt.scatter(x_blob[:, 0], x_blob[:,  1], c=y_blob, cmap =plt.cm.RdYlBu) 
+#plt.show() 
+
+# Building our multi-class classification model in PyTorch 
+device = "cuda" if torch.cuda.is_available() else "cpu" 
+
+class BlobModel(nn.Module): 
+    def __init__(self, input_features, output_features, hidden_units=8):
+        """Initializes multi-class classification model
+        Args: 
+        input_features (int): Number of input features to the model 
+        out_features (int): Number of output features (number of output classes)
+        hidden_units (int): Number of hidden units betwen layers , default 8
+
+        Returns (torch.Tensor):
+        """
+        super().__init__()
+        self.linear_layer_stack = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=output_features)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor: 
+        return self.linear_layer_stack(x) 
+    
+# Instantiate our mode  
+model_0 = BlobModel(input_features=2, 
+                    output_features=4,
+                    hidden_units=8)  
+
+print(model_0)
+
+
