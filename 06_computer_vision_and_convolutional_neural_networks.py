7b141bb4b9722ca33d2857dc9825c991fbf1264b
Alexander Komla <kunalexander360@gmail.com>
2025-02-11 14:19:39 +0000

    Evaluating our model

--- 06_computer_vision_and_convolutional_neural_networks.py
+++ 06_computer_vision_and_convolutional_neural_networks.py
@@ -309,3 +309,39 @@ end_time = timer()
 print_train_time(start_time, end_time, device=str(next(model_0.parameters()).device))
 
 
+# Make predicitions and get Model 0 results 
+torch.manual_seed(42) 
+
+def eval_model(model: torch.nn.Module, 
+                data_loader: torch.utils.data.DataLoader, 
+                loss_fn: torch.nn.Module, 
+                accuracy_fn): 
+    """Returns a dictionary containing the results of model prediction on data loader"""
+    loss, acc = 0, 0 
+    model.eval() 
+
+    with torch.inference_mode(): 
+        for x, y in tqdm(data_loader): 
+
+            # Make predictions 
+            y_pred = model(x) 
+
+            # Accumulate loss and acc per batch 
+            loss += loss_fn(y_pred, y) 
+            acc += accuracy_fn(y, y_pred.argmax(dim=1))
+        # Scale the loss and acc to find the average per batch 
+
+        loss /= len(data_loader) 
+        acc /= len(data_loader) 
+    
+    return {"model_name": model.__class__.__name__, 
+            "model_loss": loss.item(), 
+            "model_acc": acc}
+
+# Calculate model 0 results on test dataset 
+model_0_results = eval_model(model=model_0, 
+data_loader=test_dataloader, 
+loss_fn=loss_fn, 
+accuracy_fn=accuracy_fn) 
+
+print(model_0_results)
