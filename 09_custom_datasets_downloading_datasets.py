e7051cf8d37fbfe2ef7acdf9005ba7d5c2f27250
Alexander Komla <kunalexander360@gmail.com>
2025-02-17 01:27:00 +0000

    Downloading custom datasets

--- 09_custom_datasets_downloading_datasets.py
+++ 09_custom_datasets_downloading_datasets.py
@@ -47,3 +47,15 @@ if image_path.is_dir():
 else: 
     print(f"{image_path} does not exist, creating one...") 
     image_path.mkdir(parents=True, exist_ok=True) 
+
+
+# Download pizza, stake and sushi data 
+with open(data_path/"pizza_steak_sushi.zip", "wb") as f: 
+    request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
+    print("Downloading pizza, steak, sushi data ...") 
+    f.write(request.content)
+
+# Unzip pizza, steak, sushi data 
+with zipfile.ZipFile(data_path / "pizza_steak_sushi.zip", "r") as zip_ref: 
+    print("Unzipping pizza, steak and shushi data") 
+    zip_ref.extractall(image_path) 
